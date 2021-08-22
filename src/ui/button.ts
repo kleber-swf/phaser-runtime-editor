8efd43c2a948d624767161612e6927537910e063
Kleber Silva <kleber@jogoglobal.com>
2021-08-22 12:44:58 -0300

    added button with states

--- src/ui/button.ts
+++ src/ui/button.ts
@@ -6,31 +6,43 @@ export interface ButtonConfig {
 	fontSize: string;
 }
 
+export interface ButtonState {
+	fill: number;
+	alpha?: number;
+}
+
 export interface ButtonStates {
-	up: number;
-	over: number;
-	down: number;
+	up: ButtonState;
+	over: ButtonState;
+	down: ButtonState;
 }
 
 export class Button extends Phaser.Group {
 	private readonly graphics: Phaser.Graphics;
-	private readonly label: Phaser.Text;
-	private readonly config: ButtonConfig;
 
-	constructor(game: Phaser.Game, text: string, config: ButtonConfig, parent: Phaser.Group) {
+	constructor(game: Phaser.Game, text: string, private readonly config: ButtonConfig, parent: Phaser.Group) {
 		super(game, parent);
-		// this.states = config.states;
-		this.graphics = Button.createGraphics(game, config, this);
-		this.label = Button.createLabel(game, text, config, this);
+		this.graphics = this.createGraphics(game, config);
+		this.createLabel(game, text, config);
+		this.setState('up');
 	}
 
-	private static createGraphics(game: Phaser.Game, config: ButtonConfig, parent: Phaser.Group) {
-		const graphics = new Phaser.Graphics(game).beginFill(0, 1).drawRect(0, 0, config.width, config.height).endFill();
-		parent.add(graphics);
-		return graphics;
+	private createGraphics(game: Phaser.Game, config: ButtonConfig) {
+		const g = new Phaser.Graphics(game).beginFill(0, 1).drawRect(0, 0, config.width, config.height).endFill();
+		this.add(g);
+
+		g.inputEnabled = true;
+		const e = g.events;
+
+		e.onInputOver.add(() => this.setState('over'));
+		e.onInputOut.add(() => this.setState('up'));
+		e.onInputDown.add(() => this.setState('down'));
+		e.onInputUp.add(() => this.setState(g.input.pointerOver() ? 'over' : 'up'));
+
+		return g;
 	}
 
-	private static createLabel(game: Phaser.Game, text: string, config: ButtonConfig, parent: Phaser.Group) {
+	private createLabel(game: Phaser.Game, text: string, config: ButtonConfig) {
 		const px = config.width * 0.5;
 		const py = config.height * 0.5;
 		const label = new Phaser.Text(game, 0, 0, text, {
@@ -40,7 +52,15 @@ export class Button extends Phaser.Group {
 		});
 		label.anchor.set(0.5, 0.5);
 		label.position.set(px, py + 2);
-		parent.add(label);
+		this.add(label);
 		return label;
+	}
+
+	private setState(state: keyof ButtonStates) {
+		const st = this.config.states[state];
+		this.graphics.clear()
+			.beginFill(st.fill, st.alpha)
+			.drawRect(0, 0, this.config.width, this.config.height)
+			.endFill();
 	}
 }
--- src/ui/toolbar.ts
+++ src/ui/toolbar.ts
@@ -5,7 +5,11 @@ export class ToolBar extends Phaser.Group {
 		super(game, parent);
 		const button = new Button(game, 'EDIT', {
 			width: 50, height: 20,
-			states: { up: 0xBBBBBB, over: 0xFFFFFF, down: 0xCCCCCC },
+			states: {
+				up: { fill: 0x00548C, alpha: 0.5 },
+				over: { fill: 0x00548C },
+				down: { fill: 0x001F33 }
+			},
 			fontSize: '10px',
 			textColor: '#FFFFFF',
 		}, this);
