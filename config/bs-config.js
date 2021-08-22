b5fcbeca0d0adb6abc9234d8869e286989455cb4
Kleber Silva <kleber@jogoglobal.com>
2021-08-22 11:33:19 -0300

    added example project

---
+++ config/bs-config.js
@@ -0,0 +1,19 @@
+// Browser sync configuration file for the example project
+module.exports = {
+	server: ['./example'],
+	serveStatic: [
+		{
+			route: '/js',
+			dir: [
+				'./dist',
+				'./node_modules/phaser-ce/build'
+			],
+		},
+	],
+	files: [
+		'./dist/index.js',
+		'./example/**/*'
+	],
+	ghostMode: false,
+	open: false,
+};
