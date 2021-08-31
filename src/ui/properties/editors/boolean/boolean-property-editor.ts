9c956faa29073fcc999d28e7e17b8627edf46d8a
Kleber Silva <kleber.swf@gmail.com>
2021-08-30 22:07:54 -0300

    string and boolean property editors now parse data object

--- src/ui/properties/editors/boolean/boolean-property-editor.ts
+++ src/ui/properties/editors/boolean/boolean-property-editor.ts
@@ -1,14 +1,16 @@
+import { PropertyInspectionData } from 'ui/properties-editors';
 import { PropertyEditor } from '../property-editor';
 
 export class BooleanPropertyEditor extends PropertyEditor<boolean> {
 	public static readonly tagName: string = 'phed-boolean-property-editor';
 
-	protected createInnerContent(value: boolean, propertyId: string) {
+	protected createInnerContent(value: boolean, propertyId: string, prop: PropertyInspectionData) {
 		const input = document.createElement('input');
 		input.id = propertyId;
 
 		input.setAttribute('type', 'checkbox');
 		if (value) input.setAttribute('checked', '');
+		if (prop.data) Object.keys(prop.data).forEach(p => input.setAttribute(p, prop.data[p]));
 
 		return input;
 	}
