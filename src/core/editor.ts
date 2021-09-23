ffb9e124468dc64d4a004cd9fa87b0c0c6ae900e
Kleber Silva <kleber.swf@gmail.com>
2021-09-23 12:39:46 -0300

    letting editor setup its initial inspector data

--- src/core/editor.ts
+++ src/core/editor.ts
@@ -1,3 +1,4 @@
+import { ComponentTags } from 'component-tags';
 import { EditorData } from 'data/editor-data';
 import { InspectorData } from 'data/inspector-data';
 import { PhaserMeta } from 'data/phaser-meta';
@@ -18,13 +19,115 @@ class EditorClass {
 
 	public init(clearPrefs: boolean) {
 		this.data = new EditorData();
-		this.inspectorData = new InspectorData();
+		this.inspectorData = this.createInspectorData();
 		this.meta = new PhaserMeta();
 
 		this.actions = new ActionHandler();
 		this.history = new History(this.data);
 		this.prefs = new Preferences(clearPrefs);
 	}
+
+	private createInspectorData() {
+		const data = new InspectorData();
+		data.addTypeEditors({
+			// basic types
+			string: ComponentTags.StringPropertyEditor,
+			text: ComponentTags.TextPropertyEditor,
+			number: ComponentTags.NumberPropertyEditor,
+			boolean: ComponentTags.BooleanPropertyEditor,
+
+			// PIXI/Phaser types
+			point: ComponentTags.PointPropertyEditor,
+			rect: ComponentTags.RectPropertyEditor,
+
+			// default
+			default: ComponentTags.StringPropertyEditor,
+		});
+
+		data.addInspectableProperties([
+			{ name: '__type', label: 'type', typeHint: 'string', data: { readonly: true } },
+			{ name: 'name', typeHint: 'string' },
+			{ name: 'position', typeHint: 'point' },
+			{ name: 'scale', typeHint: 'point', data: { step: 0.1 } },
+			{ name: 'pivot', typeHint: 'point' },
+			{ name: 'anchor', typeHint: 'point', data: { step: 0.1 } },
+			{ name: 'alpha', typeHint: 'number', data: { min: 0, max: 1, step: 0.1 } },
+			{ name: 'visible', typeHint: 'boolean' },
+			{ name: 'angle', typeHint: 'number', data: { readonly: true } },
+			{ name: '_bounds', label: 'bounds', typeHint: 'rect', data: { readonly: true } },
+
+			// Sprite
+			{ name: 'key', typeHint: 'string' },
+			{ name: 'frameName', label: 'frame', typeHint: 'string' },
+			{ name: 'blendMode', typeHint: 'number' },
+			{ name: 'tint', typeHint: 'number', data: { min: 0, max: 0xFFFFFF } },
+
+			// Text
+			{ name: 'text', typeHint: 'text', data: { rows: 3 } },
+			{ name: 'font', typeHint: 'string' },
+			{ name: 'fontSize', typeHint: 'number', data: { min: 0, step: 1 } },
+			{ name: 'fontStyle', typeHint: 'string' },
+			{ name: 'fontVariant', typeHint: 'string' },
+			{ name: 'fontWeight', typeHint: 'string' },
+			{ name: 'autoRound', typeHint: 'boolean' },
+			{ name: 'align', typeHint: 'string' },
+			{ name: 'wordWrap', typeHint: 'boolean' },
+			{ name: 'wordWrapWidth', typeHint: 'number', data: { min: 0, step: 1 } },
+			{ name: 'useAdvancedWordWrap', typeHint: 'boolean' },
+			{ name: 'padding', typeHint: 'point' },
+			// { name: 'textBounds', typeHint: 'rect' },  // TODO waiting for null checking on rect editor
+			{ name: 'boundsAlignH', typeHint: 'string' },
+			{ name: 'boundsAlignV', typeHint: 'string' },
+
+		]);
+
+		const basicProperties = {
+			title: '', properties: [
+				'__type', 'name', 'position', 'scale', 'pivot', 'anchor',
+				'alpha', 'visible', 'angle', '_bounds'
+			]
+		};
+
+		data.addObjectProperties('default', [basicProperties]);
+
+		data.addObjectProperties('Phaser.Sprite', [
+			basicProperties,
+			{ title: 'Sprite', properties: ['key', 'frameName', 'blendMode', 'tint'] },
+		]);
+
+		data.addObjectProperties('Phaser.Image', [
+			basicProperties,
+			{ title: 'Sprite', properties: ['key', 'frameName', 'blendMode', 'tint'] },
+		]);
+
+		data.addObjectProperties('Phaser.Graphics', [
+			basicProperties,
+			{ title: 'Sprite', properties: ['blendMode', 'tint'] },
+		]);
+
+		data.addObjectProperties('Phaser.Text', [
+			basicProperties,
+			{ title: 'Sprite', properties: ['blendMode', 'tint'] },
+			{
+				title: 'Text',
+				properties: [
+					'text', 'font', 'fontSize', 'fontStyle', 'fontVariant', 'fontWeight', 'autoRound',
+					'divider',
+					'align', 'wordWrap', 'wordWrapWidth', 'useAdvancedWordWrap',
+					'divider',
+					'padding', /*'textBounds', */'boundsAlignH', 'boundsAlignV']
+			},
+		]);
+
+		data.addObjectProperties('Phaser.BitmapText', [
+			basicProperties,
+			{ title: 'Sprite', properties: ['tint'] },
+			{ title: 'Bitmap Text', properties: ['font', 'fontSize', 'align'] },
+		]);
+
+		return data;
+	}
+
 
 	public enable() {
 		this.actions.enable();
--- src/editor.window.ts
+++ src/editor.window.ts
@@ -36,9 +36,7 @@ export class EditorWindow {
 
 	private init() {
 		this._initialized = true;
-
 		Editor.init(this.config.clearPrefs);
-		this.setupInspectorData();
 
 		this.sceneView = new SceneView(this.game);
 		this.editorView = document.createElement(ComponentTags.EditorView) as EditorView;
@@ -66,105 +64,6 @@ export class EditorWindow {
 
 		this.disabledUI.enable();
 		if (this.onhide) this.onhide();
-	}
-
-	private setupInspectorData() {
-		Editor.inspectorData.addTypeEditors({
-			// basic types
-			string: ComponentTags.StringPropertyEditor,
-			text: ComponentTags.TextPropertyEditor,
-			number: ComponentTags.NumberPropertyEditor,
-			boolean: ComponentTags.BooleanPropertyEditor,
-
-			// PIXI/Phaser types
-			point: ComponentTags.PointPropertyEditor,
-			rect: ComponentTags.RectPropertyEditor,
-
-			// default
-			default: ComponentTags.StringPropertyEditor,
-		});
-
-		Editor.inspectorData.addInspectableProperties([
-			{ name: '__type', label: 'type', typeHint: 'string', data: { readonly: true } },
-			{ name: 'name', typeHint: 'string' },
-			{ name: 'position', typeHint: 'point' },
-			{ name: 'scale', typeHint: 'point', data: { step: 0.1 } },
-			{ name: 'pivot', typeHint: 'point' },
-			{ name: 'anchor', typeHint: 'point', data: { step: 0.1 } },
-			{ name: 'alpha', typeHint: 'number', data: { min: 0, max: 1, step: 0.1 } },
-			{ name: 'visible', typeHint: 'boolean' },
-			{ name: 'angle', typeHint: 'number', data: { readonly: true } },
-			{ name: '_bounds', label: 'bounds', typeHint: 'rect', data: { readonly: true } },
-
-			// Sprite
-			{ name: 'key', typeHint: 'string' },
-			{ name: 'frameName', label: 'frame', typeHint: 'string' },
-			{ name: 'blendMode', typeHint: 'number' },
-			{ name: 'tint', typeHint: 'number', data: { min: 0, max: 0xFFFFFF } },
-
-			// Text
-			{ name: 'text', typeHint: 'text', data: { rows: 3 } },
-			{ name: 'font', typeHint: 'string' },
-			{ name: 'fontSize', typeHint: 'number', data: { min: 0, step: 1 } },
-			{ name: 'fontStyle', typeHint: 'string' },
-			{ name: 'fontVariant', typeHint: 'string' },
-			{ name: 'fontWeight', typeHint: 'string' },
-			{ name: 'autoRound', typeHint: 'boolean' },
-			{ name: 'align', typeHint: 'string' },
-			{ name: 'wordWrap', typeHint: 'boolean' },
-			{ name: 'wordWrapWidth', typeHint: 'number', data: { min: 0, step: 1 } },
-			{ name: 'useAdvancedWordWrap', typeHint: 'boolean' },
-			{ name: 'padding', typeHint: 'point' },
-			// { name: 'textBounds', typeHint: 'rect' },  // TODO waiting for null checking on rect editor
-			{ name: 'boundsAlignH', typeHint: 'string' },
-			{ name: 'boundsAlignV', typeHint: 'string' },
-
-		]);
-
-		const basicProperties = {
-			title: '', properties: [
-				'__type', 'name', 'position', 'scale', 'pivot', 'anchor',
-				'alpha', 'visible', 'angle', '_bounds'
-			]
-		};
-
-		Editor.inspectorData.addObjectProperties('default', [basicProperties]);
-
-		Editor.inspectorData.addObjectProperties('Phaser.Sprite', [
-			basicProperties,
-			{ title: 'Sprite', properties: ['key', 'frameName', 'blendMode', 'tint'] },
-		]);
-
-		Editor.inspectorData.addObjectProperties('Phaser.Image', [
-			basicProperties,
-			{ title: 'Sprite', properties: ['key', 'frameName', 'blendMode', 'tint'] },
-		]);
-
-		Editor.inspectorData.addObjectProperties('Phaser.Graphics', [
-			basicProperties,
-			{ title: 'Sprite', properties: ['blendMode', 'tint'] },
-		]);
-
-		Editor.inspectorData.addObjectProperties('Phaser.Text', [
-			basicProperties,
-			{ title: 'Sprite', properties: ['blendMode', 'tint'] },
-			{
-				title: 'Text',
-				properties: [
-					'text', 'font', 'fontSize', 'fontStyle', 'fontVariant', 'fontWeight', 'autoRound',
-					'divider',
-					'align', 'wordWrap', 'wordWrapWidth', 'useAdvancedWordWrap',
-					'divider',
-					'padding', /*'textBounds', */'boundsAlignH', 'boundsAlignV']
-			},
-		]);
-
-		Editor.inspectorData.addObjectProperties('Phaser.BitmapText', [
-			basicProperties,
-			{ title: 'Sprite', properties: ['tint'] },
-			{ title: 'Bitmap Text', properties: ['font', 'fontSize', 'align'] },
-		]);
-
 	}
 
 	private setupActions(scene: SceneView) {
