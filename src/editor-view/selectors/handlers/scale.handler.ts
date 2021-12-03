9b8fddd106e142badae10713f09c2f424bd10b6d
Kleber Silva <kleber.swf@gmail.com>
2021-12-03 17:15:58 -0300

    proportional scaling with ctrl; center scale with alt

--- src/editor-view/selectors/handlers/scale.handler.ts
+++ src/editor-view/selectors/handlers/scale.handler.ts
@@ -3,16 +3,21 @@ import { HSide, ScaleGizmo, VSide } from '../gizmos/scale-gizmo';
 import { SelectionUtil } from '../selection.util';
 import { DraggingHandler } from './dragging-handler';
 
+// TODO
+//	- group
+//	- reset pivot
+
 export class ScaleHandler implements DraggingHandler {
 	private _point: Point;
 	private _object: PIXI.DisplayObject;
 	private _vside: VSide;
 	private _hside: HSide;
+	private _centered = false;
 
-	private _isGroup: boolean;
-	private _groupBoundaries = {
-		top: 0, left: 0, bottom: 0, right: 0,
-	};
+	// private _isGroup: boolean;
+	// private _groupBoundaries = {
+	// 	top: 0, left: 0, bottom: 0, right: 0,
+	// };
 
 	public startHandling(e: MouseEvent, object: PIXI.DisplayObject): void {
 		const gizmo = e.target as ScaleGizmo;
@@ -25,6 +30,7 @@ export class ScaleHandler implements DraggingHandler {
 		}
 
 		this._object = object;
+		this._centered = e.altKey;
 
 		// inverting axis
 		if (object.scale.x < 0) {
@@ -40,15 +46,12 @@ export class ScaleHandler implements DraggingHandler {
 		this._vside = vside;
 		this._hside = hside;
 
-		object.updateTransform();
+		if (this._centered) {
+			this.setPivotAndPosition(object, VSide.Middle, HSide.Center);
+		} else {
+			this.setPivotAndPosition(object, vside, hside);
+		}
 
-		const x = object.x - object.pivot.x * object.scale.x + object.width * hside;
-		const y = object.y - object.pivot.y * object.scale.y + object.height * vside;
-		const pivotx = Math.abs(object.width / object.scale.x) * hside;
-		const pivoty = Math.abs(object.height / object.scale.y) * vside;
-
-		object.pivot.set(pivotx, pivoty);
-		object.position.set(x, y);
 		object.updateTransform();
 
 		// this._isGroup = !object.anchor;
@@ -59,6 +62,16 @@ export class ScaleHandler implements DraggingHandler {
 		// 	this._groupBoundaries.right = object.right;
 		// 	// this._groupStickHorizontal = hside ===HSide.Left
 		// }
+	}
+
+	private setPivotAndPosition(object: PIXI.DisplayObject, vside: VSide, hside: HSide) {
+		const x = object.x - object.pivot.x * object.scale.x + object.width * hside;
+		const y = object.y - object.pivot.y * object.scale.y + object.height * vside;
+		const pivotx = Math.abs(object.width / object.scale.x) * hside;
+		const pivoty = Math.abs(object.height / object.scale.y) * vside;
+
+		object.pivot.set(pivotx, pivoty);
+		object.position.set(x, y);
 	}
 
 	public handle(e: MouseEvent): void {
@@ -79,15 +92,33 @@ export class ScaleHandler implements DraggingHandler {
 		else if (this._vside === VSide.Bottom) dy = newPoint.y - lastPoint.y;
 		this._point = newPoint;
 
-		this._object.updateTransform();
-		this._object.width += dx;
-		this._object.height += dy;
+		const obj = this._object;
 
+		if (e.altKey) {
+			if (!this._centered) {
+				this.setPivotAndPosition(this._object, VSide.Middle, HSide.Center);
+			}
+		} else if (this._centered) {
+			this.setPivotAndPosition(this._object, this._vside, this._hside);
+		}
+
+		this._centered = e.altKey;
+
+		if (e.ctrlKey) {
+			const ratio = this._hside === 0.5
+				? (obj.height + dy) / obj.height
+				: (obj.width + dx) / obj.width;
+			obj.width *= ratio;
+			obj.height *= ratio;
+		} else {
+			obj.width += dx;
+			obj.height += dy;
+		}
 		// if (this._isGroup) {
-		// 	this._object.top = this._top;
-		// 	this._object.left = this._left;
+		// 	obj.top = this._top;
+		// 	obj.left = this._left;
 		// }
-		this._object.updateTransform();
+		obj.updateTransform();
 	}
 
 	public stopHandling(): void {
